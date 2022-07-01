7047d19acbc8116440e7476385583d80153361b3
Vimal <vimal.nair@redteclab.com>
2022-07-01 17:19:41 +0200

    feat: adding charts and style

---
+++ src/components/Chart/Chart.js
@@ -0,0 +1,17 @@
+import './Chart.css'
+import ChartBar from "./ChartBar";
+
+const Chart = (props) => {
+
+    return (
+        <div className='chart'>
+            {props.dataPoints.map((dataPoint) => {
+                                     return <ChartBar key={dataPoint.label} value={dataPoint.value} maxValue={props.maxValue} label={dataPoint.label}/>
+                                  }
+            )}
+        </div>
+    )
+
+}
+
+export default Chart
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -4,10 +4,31 @@ import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
 import './Expenses.css';
 import ExpensesFilter from "../NewExpenseFilter/ExpensesFilter";
+import Chart from "../Chart/Chart";
 
 const Expenses = (props) => {
+    const dataPoints = [
+        {'label': 'Jan', 'value': 0},
+        {'label': 'Feb', 'value': 0},
+        {'label': 'Mar', 'value': 0},
+        {'label': 'Apr', 'value': 0},
+        {'label': 'May', 'value': 0},
+        {'label': 'Jun', 'value': 0},
+        {'label': 'Jul', 'value': 0},
+        {'label': 'Aug', 'value': 0},
+        {'label': 'Sep', 'value': 0},
+        {'label': 'Oct', 'value': 0},
+        {'label': 'Nov', 'value': 0},
+        {'label': 'Dec', 'value': 0},
+    ]
+    props.items.forEach(item => {
+        dataPoints[item.date.getMonth()].value+=item.amount})
+
+    const eachMonthExpense = dataPoints.map(dataPoint => dataPoint.value)
+    const maximumExpenseOfTheYear = Math.max(...eachMonthExpense)
     return (
         <div>
+            <Chart dataPoints={dataPoints} maxValue={maximumExpenseOfTheYear}/>
             <Card className="expenses">
                 <ExpensesFilter selected={props.selected} onSelectingFilter={props.onSelectingFilter}/>
                 {props.items.map((expenses) => (
